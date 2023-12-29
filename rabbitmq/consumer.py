ce646c8f8cfff79e230bdf928287339fd0d31fcc
IbrahimOdaci <ibrahim.odaci.1@gmail.com>
2023-12-29 20:45:52 +0300

    Publisher and consumer apps running stable

---
+++ rabbitmq/consumer.py
@@ -0,0 +1,25 @@
+import pika,json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+queue = channel.queue_declare('devops')
+queue_name = queue.method.queue
+
+channel.exchange_declare(exchange='excdefault',exchange_type='direct',durable=True)
+
+channel.queue_bind(
+    exchange='excdefault',
+    queue=queue_name,
+    routing_key='devops'
+)
+
+def callback(ch, method,properties,body):
+    print('=>Receiving:',body)
+    payload = body#.decode('utf8')  #json.loads(body.decode('utf8'))
+    # print('Message received: {}'.format(payload['message']))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_consume(on_message_callback=callback,queue=queue_name)
+print(' Waiting for incoming messages..')
+channel.start_consuming()
--- rabbitmq/producer.py
+++ rabbitmq/producer.py
@@ -1,22 +1,28 @@
-import pika, json,uuid
+import pika,time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel= connection.channel()
 
-message = 'Ibrahim test message'
+#message to be published
+message='ibrahim test message'
+
+#Creating the exchange and specifying the exchange type as direct.
 channel.exchange_declare(
-        exchange='order',
-        exchange_type='direct'
+        exchange='excdefault',
+        exchange_type='direct',
+        durable=True
 )
 
-# publish the message. since we have a single queue, using routing_key is optional
-channel.basic_publish(
-    exchange='order',
-    routing_key='devops',
-    body=message
-)
-# Print the message on the screen
-print(f'A message is sent: {0} ',message)
+# publishing the message. since we have a single queue, using routing_key is optional
+# it will publish a message endlessly each in 3 seconds intervals
+while 1:
+    time.sleep(3)
+    print("=> Sending: ", message)
+    channel.basic_publish(
+        exchange='excdefault',
+        routing_key='devops',
+        body=message
+    )
 
 # Close the connection
 connection.close()
